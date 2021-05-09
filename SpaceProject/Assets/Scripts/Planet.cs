fb1694aa4f61f21f3411fa8bb2a50ba6b8b64b9d
MisterMarv <gabriel.marvin@nave.org.br>
2021-05-08 23:22:37 -0300

    FirstProjectOfPlanet
    ° Mainly Update --> Sphere with mesh

---
+++ SpaceProject/Assets/Scripts/Planet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2,256)]
+    public int resolutionFace = 10;
+    
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    void Initialize()
+    {
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int triVertex = 0; triVertex < 6; triVertex++)
+        {
+            if (meshFilters[triVertex] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[triVertex] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[triVertex].sharedMesh = new Mesh();
+            }
+            terrainFaces[triVertex] = new TerrainFace(meshFilters[triVertex].sharedMesh, resolutionFace, directions[triVertex]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+}
