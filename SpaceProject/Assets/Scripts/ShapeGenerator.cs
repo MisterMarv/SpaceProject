9acacb397b6ed5d244fa8f204d7203a406f37b2d
MisterMarv <gabriel.marvin@nave.org.br>
2021-05-10 12:35:44 -0300

    SaveUpdate | LayeredNoise

--- SpaceProject/Assets/Scripts/ShapeGenerator.cs
+++ SpaceProject/Assets/Scripts/ShapeGenerator.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class ShapeGenerator 
 {
     ShapeSettings settings;
+    NoiseFilter[] noiseFilters;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+        noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+
+        for (int processFilters = 0; processFilters < noiseFilters.Length; processFilters++)
+        {
+            noiseFilters[processFilters] = new NoiseFilter(settings.noiseLayers[processFilters].noiseSettings);
+        }
     }
     
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        return pointOnUnitSphere * settings.planetRadius;
+        float elevation = 0;
+        for (int processElevation = 0; processElevation < noiseFilters.Length; processElevation++)
+        {
+            if (settings.noiseLayers[processElevation].enabled)
+            {
+                elevation += noiseFilters[processElevation].Evaluate(pointOnUnitSphere);
+            }
+        }
+        return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
     }
 }
